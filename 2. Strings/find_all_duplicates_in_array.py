0a277299aa8c96df4107dae3c5e4831447f246a8
ranjeet-snu <107781937+ranjeet-snu@users.noreply.github.com>
2022-09-22 12:54:27 +0530

    Delete find_all_duplicates_in_array.py

--- 2. Strings/find_all_duplicates_in_array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        li=[]
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]==nums[j]:
-                    li.append(nums[i])
-        return li
